fc32654b26df3902c7f06c389acddb359d5e0170
ldkk <ldk>
2017-12-10 20:38:54 +0800

    商品全部

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,7 +2,37 @@ angular.module('starter.controllers', [])
 
 .controller('DashCtrl', function($scope) {})
 
-.controller('ChatsCtrl', function($scope, Chats) {
+.controller('ChatsCtrl', function($scope, Chats,$rootScope) {
+  $rootScope.shopguqin = [
+    {name:'古琴',id:'1',pho:'../img/qin.jpg',til:'精美古琴',yuan:'￥8888',carimg:'../img/shopcar.jpg',dianpu:'优乐馆',jianjie:'精美古琴，价格便宜，质量上乘，采用顶级大师技艺制作，音感极好，是爱琴人士的不二之选'},
+    {name:'古琴',id:'2',pho:'../img/guqin2.jpg',til:'精美古琴',yuan:'￥8888',carimg:'../img/shopcar.jpg',dianpu:'优乐馆',jianjie:'精美古琴，价格便宜，质量上乘，采用顶级大师技艺制作，音感极好，是爱琴人士的不二之选'}
+  ];
+  $rootScope.shopguqin2 = [
+    {name:'古琴',id:'1',pho:'../img/qin.jpg',til:'精美古琴',yuan:'￥8888',carimg:'../img/shopcar.jpg',dianpu:'优乐馆',jianjie:'精美古琴，价格便宜，质量上乘，采用顶级大师技艺制作，音感极好，是爱琴人士的不二之选'},
+    {name:'古琴',id:'2',pho:'../img/guqin2.jpg',til:'精美古琴',yuan:'￥8888',carimg:'../img/shopcar.jpg',dianpu:'优乐馆',jianjie:'精美古琴，价格便宜，质量上乘，采用顶级大师技艺制作，音感极好，是爱琴人士的不二之选'},
+    {name:'古琴',id:'3',pho:'../img/guqin3.jpg',til:'精美古琴',yuan:'￥9999',carimg:'../img/shopcar.jpg',dianpu:'优乐馆',jianjie:'精美古琴，价格便宜，质量上乘，采用顶级大师技艺制作，音感极好，是爱琴人士的不二之选'}
+  ];
+
+  $rootScope.shopgq=[
+    {name:'钢琴',id:'1',pho:'../img/gangqin1.jpg',til:'优选钢琴',yuan:'￥6666',carimg:'../img/shopcar.jpg',dianpu:'优乐馆',jianjie:'精美钢琴，价格便宜，质量上乘，采用顶级大师技艺制作，音感极好，是爱琴人士的不二之选'},
+    {name:'钢琴',id:'2',pho:'../img/gangqin2.jpg',til:'优选钢琴',yuan:'￥6666',carimg:'../img/shopcar.jpg',dianpu:'优乐馆',jianjie:'精美钢琴，价格便宜，质量上乘，采用顶级大师技艺制作，音感极好，是爱琴人士的不二之选'}
+  ];
+  $rootScope.shopgq2=[
+    {name:'钢琴',id:'1',pho:'../img/gangqin1.jpg',til:'优选钢琴',yuan:'￥6666',carimg:'../img/shopcar.jpg',dianpu:'优乐馆',jianjie:'精美钢琴，价格便宜，质量上乘，采用顶级大师技艺制作，音感极好，是爱琴人士的不二之选'},
+    {name:'钢琴',id:'2',pho:'../img/gangqin2.jpg',til:'优选钢琴',yuan:'￥6666',carimg:'../img/shopcar.jpg',dianpu:'优乐馆',jianjie:'精美钢琴，价格便宜，质量上乘，采用顶级大师技艺制作，音感极好，是爱琴人士的不二之选'},
+    {name:'钢琴',id:'3',pho:'../img/gangqin3.jpg',til:'优选钢琴',yuan:'￥6688',carimg:'../img/shopcar.jpg',dianpu:'优乐馆',jianjie:'精美钢琴，价格便宜，质量上乘，采用顶级大师技艺制作，音感极好，是爱琴人士的不二之选'}
+  ];
+
+  $rootScope.shoptq=[
+    {name:'提琴',id:'1',pho:'../img/tiqin1.jpg',til:'优质提琴',yuan:'￥666',carimg:'../img/shopcar.jpg',dianpu:'优乐馆',jianjie:'精美提琴，价格便宜，质量上乘，采用顶级大师技艺制作，音感极好，是爱琴人士的不二之选'},
+    {name:'提琴',id:'2',pho:'../img/tiqin2.jpg',til:'优质提琴',yuan:'￥888',carimg:'../img/shopcar.jpg',dianpu:'优乐馆',jianjie:'精美提琴，价格便宜，质量上乘，采用顶级大师技艺制作，音感极好，是爱琴人士的不二之选'}
+   ];
+  $rootScope.shoptq2=[
+    {name:'提琴',id:'1',pho:'../img/tiqin1.jpg',til:'优质提琴',yuan:'￥666',carimg:'../img/shopcar.jpg',dianpu:'优乐馆',jianjie:'精美提琴，价格便宜，质量上乘，采用顶级大师技艺制作，音感极好，是爱琴人士的不二之选'},
+    {name:'提琴',id:'2',pho:'../img/tiqin2.jpg',til:'优质提琴',yuan:'￥888',carimg:'../img/shopcar.jpg',dianpu:'优乐馆',jianjie:'精美提琴，价格便宜，质量上乘，采用顶级大师技艺制作，音感极好，是爱琴人士的不二之选'},
+    {name:'提琴',id:'3',pho:'../img/tiqin3.jpg',til:'优质提琴',yuan:'￥999',carimg:'../img/shopcar.jpg',dianpu:'优乐馆',jianjie:'精美提琴，价格便宜，质量上乘，采用顶级大师技艺制作，音感极好，是爱琴人士的不二之选'}
+  ];
+
   $scope.chats = Chats.all();
   $scope.remove = function(chat) {
     Chats.remove(chat);
--- www/templates/shop/shop-js/shopController.js
+++ www/templates/shop/shop-js/shopController.js
@@ -1,4 +1,26 @@
 angular.module('shop.controllers', [])
-  .controller('shopCtrl', function($scope) {
+  .controller('shopCtrl', function($scope) {})//精选
+  .controller('guqin1Ctrl', function($scope, $stateParams,$rootScope) {
 
-});
+
+    if ($stateParams.name=='shopguqin'){
+      $scope.data = $rootScope.shopguqin[$stateParams.id-1];
+    } else if ($stateParams.name=='shopgq'){
+      $scope.data = $rootScope.shopgq[$stateParams.id-1];
+    } else if($stateParams.name=='shoptq'){
+      $scope.data = $rootScope.shoptq[$stateParams.id-1];
+    }
+
+
+
+
+  })
+  .controller('guqinCtrl', function($scope, $stateParams, Chats) {
+
+  })
+  .controller('gangqinCtrl', function($scope, $stateParams, Chats) {
+
+  })
+  .controller('tiqinCtrl', function($scope, $stateParams, Chats) {
+
+  });
